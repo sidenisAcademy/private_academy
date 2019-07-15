7d9aba51a9d404f645a3002330bb9433c57e8ed1
Alexander.Cherkashin <alexander.cherkashin@sidenis.com>
2019-07-15 13:40:40 +0700

    Adding Tax-Calculator2

--- src/main/java/Cherkashin/TestTax.java
+++ src/main/java/Cherkashin/TestTax.java
@@ -2,6 +2,7 @@ package Cherkashin;
 
 public class TestTax {
     public static void main(String[] args) {
+        USATax usaTax = new USATax();
         Tax tax = new Tax();
         tax.grossIncome = 1000;
         tax.city = "Tomsk";
@@ -10,7 +11,6 @@ public class TestTax {
 
         System.out.println("My taxes is " + myTaxesRus + " in " + tax.city);
 
-        USATax usaTax = new USATax();
         usaTax.grossIncome = 510000;
         usaTax.city = "NewYork";
         usaTax.dependents = 0;
@@ -18,5 +18,10 @@ public class TestTax {
         double myTaxesUsa = usaTax.calcTax();
 
         System.out.println("My taxes is " + myTaxesUsa + " in " + usaTax.city);
+
+        usaTax.grossIncome = 1000;
+        System.out.println(usaTax.calcTax(0.6, "NewYork"));
+        System.out.println(usaTax.calcTax("NewYork"));
+        System.out.println(usaTax.calcTax(0.4, 1234));
     }
 }
--- src/main/java/Cherkashin/USATax.java
+++ src/main/java/Cherkashin/USATax.java
@@ -13,4 +13,16 @@ public class USATax extends Tax {
         else return grossIncome * 0.08;
 
     }
+    public double calcTax(double taxRate, String city) {
+        System.out.println("First method: " + city);
+        return grossIncome * taxRate;
+    }
+    public double calcTax(String city) {
+        System.out.println("Second method: " + city);
+        return grossIncome*0.33;
+    }
+    public double calcTax(double taxRate, int cityCode) {
+        System.out.println("Third method: " + cityCode + " and " + taxRate);
+        return  grossIncome* taxRate;
+    }
 }
