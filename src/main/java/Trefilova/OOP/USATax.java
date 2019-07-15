734941ef7304d54943907e9171a8e5742a8b4743
AlenaQA <alyona.trefilova@gmailcom>
2019-07-15 13:25:25 +0700

    Add overloaded methods.

--- src/main/java/Trefilova/OOP/USATax.java
+++ src/main/java/Trefilova/OOP/USATax.java
@@ -14,4 +14,19 @@ public class USATax extends Tax {
             return grossIncome * 0.08;
         }
     }
+
+    public double calcTax(double tax, String city) {
+        System.out.println("Taxes for " + city + " is " + grossIncome * tax);
+        return grossIncome * tax;
+    }
+
+    public double calcTax(String city) {
+        System.out.println("Taxes for " + city + " is " + grossIncome * 0.09);
+        return grossIncome * 0.09;
+    }
+
+    public double calcTax(double tax, int code) {
+        System.out.println("Taxes for city with code " + code + " is " + grossIncome * tax);
+        return grossIncome * tax;
+    }
 }
