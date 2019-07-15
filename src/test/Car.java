2691d1385fdcaa49ef4b68efb98722b4e8e24626
sidenisAcademy <123QWE123qwe1>
2019-07-15 11:37:40 +0700

    add class Car and test

---
+++ src/test/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    String color;
+    int numberOfDoors;
+
+    void startEngine(){
+        System.out.println("Wrrooom");
+    }
+    void stopEngine(){
+        System.out.println("Stopped");
+    }
+}
