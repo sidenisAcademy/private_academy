2ac22e4bb525cbb0f91f4c3f6da592569d1f8948
Ekaterina.Cherednik <kinder_che@mail.ru>
2019-07-15 11:36:11 +0700

    Git add "Hello World"

---
+++ src/main/java/Cherednik/OOP/Car.java
@@ -0,0 +1,15 @@
+package Cherednik.OOP;
+
+public class Car {
+    String color;
+    int numberOfDoors;
+
+    void startEngine(){
+        System.out.println("Engine Started");
+    }
+
+    void stopEngine(){
+        int temp = 0;
+        System.out.println("Engine stopped");
+    }
+}
