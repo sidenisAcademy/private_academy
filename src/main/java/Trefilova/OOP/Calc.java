ab2d831529409b0c5813edf38d9f9fe32201af17
AlenaQA <alyona.trefilova@gmailcom>
2019-07-16 10:14:33 +0700

    Add Calc and TestCalc classes.

---
+++ src/main/java/Trefilova/OOP/Calc.java
@@ -0,0 +1,20 @@
+package Trefilova.OOP;
+
+public class Calc {
+
+    public double sumTwo (double a, double b) {
+        return a + b;
+    }
+
+    public double sumThree(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public double division (double a, double b) {
+        return a / b;
+    }
+
+    public double multiplication (double a) {
+        return a * a;
+    }
+}
