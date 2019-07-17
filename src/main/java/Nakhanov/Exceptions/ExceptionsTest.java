5aac5454c0c1fe5ad38121aa2e38c1adb1b3cc80
Vitalii <makaruba@mail.ru>
2019-07-17 13:59:47 +0700

    add Exception

--- src/main/java/Nakhanov/Exceptions/ExceptionsTest.java
+++ src/main/java/Nakhanov/Exceptions/ExceptionsTest.java
@@ -7,30 +7,37 @@ public class ExceptionsTest {
     public static void openFile(File file) throws IOException {
 
         FileInputStream reader = new FileInputStream(file);
+
         int i = 0;
 
         while ((i = reader.read()) != -1) {
 
             System.out.print((char) i);
         }
+        System.out.println("\n");
+        reader.close();
     }
 
     public static void openFile1(File file) {
 
-        try {
-            FileInputStream reader = new FileInputStream(file);
+        try (FileInputStream reader = new FileInputStream(file)) {
+
             int i = 0;
 
             while ((i = reader.read()) != -1) {
-
                 System.out.print((char) i);
+
             }
+
 
         } catch (FileNotFoundException e) {
             System.out.println("File is not found " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Input Error " + e.getMessage());
         }
+
+
     }
 }
+
 
--- src/main/java/Nakhanov/Exceptions/Main.java
+++ src/main/java/Nakhanov/Exceptions/Main.java
@@ -38,6 +38,7 @@ public class Main {
             System.out.println("Error " + e.getLocalizedMessage());
         }
 
+
         openFile1(file);
 
     }
