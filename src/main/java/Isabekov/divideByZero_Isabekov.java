6ab8613e5d5ceef951dcf79ef3f3e871b436ba6a
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-07-17 12:56:50 +0700

    Task1 string replace + map + divideByZero

---
+++ src/main/java/Isabekov/divideByZero_Isabekov.java
@@ -0,0 +1,25 @@
+package Isabekov;
+
+public class divideByZero_Isabekov {
+    static int divideByZero()
+    {
+        // Purposely dividing by zero
+        return 25/0;
+    }
+
+    public static int devideByZero2() {
+        try {
+            return 25/0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        return 10;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(devideByZero2());
+        }
+    }
+
+
