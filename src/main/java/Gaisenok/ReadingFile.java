530fb96532531dbd6ecbe6d75fb07869e93dec29
Alexander Gaisenok <Alexander.Gaisenok@sidenis.com>
2019-07-17 13:44:28 +0700

    work with try with resources and Reading File

--- src/main/java/Gaisenok/ReadingFile.java
+++ src/main/java/Gaisenok/ReadingFile.java
@@ -2,29 +2,23 @@ package Gaisenok;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ReadingFile {
     void readFiles(String fileName) throws IOException {
 
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        FileInputStream myFile = new FileInputStream(new File(classLoader.getResource(fileName).getFile()));
-        try {
+
+        try (FileInputStream myFile = new FileInputStream
+                (new File(classLoader
+                        .getResource(fileName)
+                        .getFile()));) {
             int content;
             while ((content = myFile.read()) != -1) {
 
                 System.out.print((char) content);
             }
         } catch (IOException e) {
-            e.getStackTrace();
-        } finally {
-            try {
-                if (myFile != null)
-                    myFile.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            e.getStackTrace();}
         }
     }
-}
