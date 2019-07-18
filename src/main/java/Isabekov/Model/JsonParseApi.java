f5aa7bf7eb5db58112a26970d89a0f9dd487cc48
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-07-19 00:57:47 +0700

    Task1 QA Academy day 4

--- src/main/java/Isabekov/Model/JsonParseApi.java
+++ src/main/java/Isabekov/Model/JsonParseApi.java
@@ -10,12 +10,32 @@ public class JsonParseApi {
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-//        DTO1 FromJson = objectMapper.readValue(new File("target/YouTube.json"), DTO1.class);
-//        System.out.println(FromJson.items);
+        DTO1 FromJson = objectMapper.readValue(new File("target/YouTube.json"), DTO1.class);
+        System.out.println(FromJson.etag);
 
-        Items FromJson = objectMapper.readValue(new File("target/YouTube.json"), Items.class);
-        System.out.println(FromJson.items);
+//        Items FromJson = objectMapper.readValue(new File("target/YouTube.json"), Items.class);
+//        System.out.println(FromJson.items);
 //        FromJson.items.forEach();
+        System.out.println();
+
+        Methods method = new Methods();
+        method.calcResults(DTO1);
+
+        System.out.println("\nSingers:");
+
+        method.printSingers(DTO1);
+
+        System.out.println("\nSingers and songs:");
+
+        method.printSingerNameAndSong(DTO1);
+
+        System.out.println();
+
+        method.getVideoId(DTO1);
+
+        System.out.println();
+
+        method.compareDates(DTO1);
 
     }
 }
---
+++ src/main/java/Isabekov/Model/Methods.java
@@ -0,0 +1,49 @@
+package Isabekov.Model;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Methods {
+    public void calcResults(DTO1 youTubeResponse) {
+        System.out.println("Number of coincidences = " + youTubeResponse.pageInfo.totalResults);
+    }
+
+    public void printSingers(DTO1 youTubeResponse) {
+        for (Items item : youTubeResponse.items)
+            System.out.println(item.singer.replaceAll("youtube#", ""));
+    }
+
+    public void printSingerNameAndSong(DTO1 youTubeResponse) {
+        for (Items item : youTubeResponse.items)
+            System.out.println(item.singer.replaceAll("youtube#", "") + " - " + item.id.song);
+    }
+
+    public void getVideoId(DTO1 youTubeResponse) {
+        for (Items item : youTubeResponse.items) {
+            if (item.id.song.contains("Bad Guy"))
+                System.out.println("Bad Guy video ID is " + item.id.videoId);
+        }
+    }
+
+    public void compareDates(DTO1 youTubeResponse) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String substring = youTubeResponse.pageInfo.data.substring(0, 10);
+
+        Date date = sdf.parse(substring);
+        LocalDate todayDate = LocalDate.now();
+        Date today = java.sql.Date.valueOf(todayDate);
+        System.out.println(sdf.format(date));
+        System.out.println(todayDate);
+
+        if (date.compareTo(today) > 0)
+            System.out.println("Today is less");
+        else if (date.compareTo(today) < 0)
+            System.out.println("Today is more");
+        else if (date.compareTo(today) == 0)
+            System.out.println("Days are equal");
+
+    }
+}
