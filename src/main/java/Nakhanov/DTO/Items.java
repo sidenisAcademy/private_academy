f6da7bce4dbd4ddd8db0210f681920f36847396c
Vitalii <makaruba@mail.ru>
2019-07-18 13:05:56 +0700

    Json parsing

---
+++ src/main/java/Nakhanov/DTO/Items.java
@@ -0,0 +1,10 @@
+package Nakhanov.DTO;
+
+
+public class Items {
+
+    public String kind;
+    public String singer;
+    public String etag;
+    public Id id;
+}
---
+++ src/main/java/Nakhanov/DTO/MainJson.java
@@ -0,0 +1,14 @@
+package Nakhanov.DTO;
+
+import java.util.List;
+
+public class MainJson {
+
+    public String category;
+    public String etag;
+    public String nextPageToken;
+    public String regionCode;
+    public PageInfo pageInfo;
+    public List<Items> items;
+
+}
