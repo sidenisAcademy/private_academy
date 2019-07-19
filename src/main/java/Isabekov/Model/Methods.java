50e828c9702f166f948850a5338451e775b4c056
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-07-19 09:55:51 +0700

    Task1 QA Academy day 4

--- src/main/java/Isabekov/Model/Methods.java
+++ src/main/java/Isabekov/Model/Methods.java
@@ -7,30 +7,30 @@ import java.util.Date;
 
 
 public class Methods {
-    public void calcResults(DTO1 youTubeResponse) {
-        System.out.println("Number of coincidences = " + youTubeResponse.pageInfo.totalResults);
+    public void calcResults(DTO1 DTO1) {
+        System.out.println("Number of coincidences = " + DTO1.pageInfo.totalResults);
     }
 
-    public void printSingers(DTO1 youTubeResponse) {
-        for (Items item : youTubeResponse.items)
+    public void printSingers(DTO1 DTO1) {
+        for (Items item : DTO1.items)
             System.out.println(item.singer.replaceAll("youtube#", ""));
     }
 
-    public void printSingerNameAndSong(DTO1 youTubeResponse) {
-        for (Items item : youTubeResponse.items)
+    public void printSingerNameAndSong(DTO1 DTO1) {
+        for (Items item : DTO1.items)
             System.out.println(item.singer.replaceAll("youtube#", "") + " - " + item.id.song);
     }
 
-    public void getVideoId(DTO1 youTubeResponse) {
-        for (Items item : youTubeResponse.items) {
+    public void getVideoId(DTO1 DTO1) {
+        for (Items item : DTO1.items) {
             if (item.id.song.contains("Bad Guy"))
                 System.out.println("Bad Guy video ID is " + item.id.videoId);
         }
     }
 
-    public void compareDates(DTO1 youTubeResponse) throws ParseException {
+    public void compareDates(DTO1 DTO1) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String substring = youTubeResponse.pageInfo.data.substring(0, 10);
+        String substring = DTO1.pageInfo.data.substring(0, 10);
 
         Date date = sdf.parse(substring);
         LocalDate todayDate = LocalDate.now();
