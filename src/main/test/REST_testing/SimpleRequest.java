d6e0d7528a2248b8aa77e351f7e1a7e99e649684
Dilaila <913953@gmail.com>
2019-07-21 21:02:18 +0700

    Add maven dependencies, simple REST request and some links

---
+++ src/main/test/REST_testing/SimpleRequest.java
@@ -0,0 +1,28 @@
+package REST_testing;
+
+import com.jayway.restassured.http.ContentType;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+
+@Slf4j
+public class SimpleRequest {
+
+    private void request() {
+        String resp = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("https://petstore.swagger.io/v2/pet/144")
+                .then()
+                .statusCode(200)
+                .extract()
+                .asString();
+        log.info("Response from https://petstore.swagger.io/v2/pet/144: " + resp);
+    }
+
+    @Test
+    public void requestTest() {
+        request();
+    }
+}
