cf0255364e9d5bfc21088d66cb0bfe3c56c989c1
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-07-22 21:44:36 +0700

    Task1 QA Academy day 6 (REST)

--- src/main/java/Isabekov/Day6/PetDto.java
+++ src/main/java/Isabekov/Day6/PetDto.java
@@ -4,10 +4,9 @@ import lombok.Getter;
 
 @Getter //обращается ко всем свойствам и get  -далее все значения будут передаваться
 public class PetDto {
-int id;
-public Category category; //публичное поле класс, которое имеет тип Кат и имя кат
-String name;
-public String[] photoUrls;
-public Category[] tags; // Category в этом случае представляется как массив
-String status;
+    public int id;
+    public Category category; //публичное поле класс, которое имеет тип Кат и имя катpublic String name;
+    public String[] photoUrls;
+    public Category[] tags; // Category в этом случае представляется как массив
+    public String status;
 }
