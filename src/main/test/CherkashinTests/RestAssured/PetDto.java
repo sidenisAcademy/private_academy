e09c0c3cbb86446235da58d190fdaf01e17bdf4f
ch11ps <sashqee@gmail.com>
2019-07-24 10:14:20 +0700

    RestAssured learning day2

--- src/main/test/CherkashinTests/RestAssured/PetDto.java
+++ src/main/test/CherkashinTests/RestAssured/PetDto.java
@@ -1,10 +1,19 @@
 package CherkashinTests.RestAssured;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class PetDto {
     public int id;
     public Category category;
     public String name;
     public String[] photoUrls;
-    public Tags[] tags;
+    public List<Tags> tags;
     public String status;
 }
