fddb17a234e8aa494bc3bc6ad4c90482938542aa
AlenaQA <alyona.trefilova@gmail.com>
2019-07-29 13:40:01 +0700

    Streams.

--- src/main/java/Trefilova/dto/Id.java
+++ src/main/java/Trefilova/dto/Id.java
@@ -1,6 +1,14 @@
 package Trefilova.dto;
 
 public class Id {
+    public String getSong() {
+        return song;
+    }
+
+    public String getVideoId() {
+        return videoId;
+    }
+
     public String song;
     public String videoId;
 }
--- src/main/java/Trefilova/dto/Items.java
+++ src/main/java/Trefilova/dto/Items.java
@@ -1,7 +1,26 @@
 package Trefilova.dto;
 
 public class Items {
+    public String getKind() {
+        return kind;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public String getEtag() {
+        return etag;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
     public String kind;
+    public String singer;
+    public String etag;
+    public Id id;
 
     @Override
     public String toString() {
@@ -13,7 +32,4 @@ public class Items {
                 '}';
     }
 
-    public String singer;
-    public String etag;
-    public Id id;
 }
