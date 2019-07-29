b440bc2812266726b84d2525f64d7ac208eb8c23
Olga.Mityagina <extern.mityagina_olga@allianz.com>
2019-07-29 13:26:49 +0700

    OOP Java Core  - Olga

---
+++ src/main/java/Olga/OOP/Car.java
@@ -0,0 +1,16 @@
+package Olga.OOP;
+
+public class Car {
+
+    String color;
+    int numOfDoors;
+
+    void startEngine() {
+        System.out.println("Engine started");
+    }
+
+    void stopEngine() {
+        int temp = 0;
+        System.out.println("Engine stopped");
+    }
+}
---
+++ src/main/java/Olga/OOP/Tax.java
@@ -0,0 +1,10 @@
+package Olga.OOP;
+
+public class Tax {
+    double grossIncome;
+    String city;
+
+    public double caclTax() {
+        return grossIncome*0.13;
+    }
+}
