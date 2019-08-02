4242b606a4544ae702af41bfe6b9ef3eb3698eee
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-08-02 13:40:01 +0700

    Merge branch 'master' of https://github.com/sidenisAcademy/private_academy into SQL_Isabekov
    # Conflicts:
    #	pom.xml
    #	src/main/java/Isabekov/YouTubeParsing/MyDto.java

--- src/main/java/Isabekov/Day15/Selenium.java
+++ src/main/java/Isabekov/Day15/Selenium.java
@@ -25,5 +25,24 @@ public class Selenium {
         System.out.println("Page title is: " + driver.getTitle());
         driver.close();
     }
+    @Test
+    public void secondUITest() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Emil.Isabekov\\Desktop\\JAVA\\REP\\privat_academy\\src\\main\\resources/chromedriver.exe"); //создаем наш драйвер на основе WebDrivre
+        WebDriver driver = new ChromeDriver(); // наш новый драйвер
+        driver.get("http://www.google.com/ru");
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+          WebElement element = driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")); // ищем элемент для того чтобы начать с ним потом взаиможействовать
+ //       WebElement element = driver.findElement(By.cssSelector("div.FPdoLc.VlcLAe > center > input.RNmpXc"));
+        element.sendKeys("Selenide");  //отправляем строку
+        element.submit();
+        System.out.println("Page title is: " + driver.getTitle());
+        //       element.click();
+//        element.sendKeys(Keys.ENTER);
+        WebElement element1 = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/div/div/div[1]/a/h3"));
+        element1.click();
+   //      element1.submit();  // можно отправить даже ctrl, alt
+        System.out.println("Page title is: " + driver.getCurrentUrl());
+       driver.close();
+    }
 
 }
