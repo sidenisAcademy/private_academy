f150b6b342c056b0139db69c81541fbf0e1986c8
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-08-02 13:07:23 +0700

    Merge branch 'master' of https://github.com/sidenisAcademy/private_academy into SQL_Isabekov
    # Conflicts:
    #	pom.xml
    #	src/main/java/Isabekov/YouTubeParsing/MyDto.java

--- src/main/java/Isabekov/Day15/Selenium.java
+++ src/main/java/Isabekov/Day15/Selenium.java
@@ -2,22 +2,28 @@ package Isabekov.Day15;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Selenium {
     @Test
     public void firstUITest() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Emil.Isabekov\\Desktop\\JAVA\\REP\\privat_academy\\src\\main\\resources/chromedriver.exe"); //создаем наш драйвер на основе WebDrivre
         WebDriver driver = new ChromeDriver(); // наш новый драйвер
-        driver.get("http://www.google.com");
+        driver.get("http://www.google.com/ru");
+//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
  //       WebElement element = driver.findElement(By.name("q")); // ищем элемент для того чтобы начать с ним потом взаиможействовать
-        WebElement element = driver.findElement(By.cssSelector("body.hp.vasq.big:nth-child(2) div.ctr-p:nth-child(4) div.jhp.big:nth-child(4) form.tsf.nj:nth-child(5) div:nth-child(2) div.A8SBwf div.FPdoLc.VlcLAe:nth-child(4) center:nth-child(1) > input.RNmpXc"));
+        WebElement element = driver.findElement(By.cssSelector("div.FPdoLc.VlcLAe > center > input.RNmpXc"));
  //       element.sendKeys("Cheese!");  //отправляем строку
-        element.submit();  // можно отправить даже ctrl, alt
+        element.click();
+//        element.sendKeys(Keys.ENTER);
+//        element.submit();  // можно отправить даже ctrl, alt
         System.out.println("Page title is: " + driver.getTitle());
- //       driver.close();
+        driver.close();
     }
 
 }
