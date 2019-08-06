bd25b4e86c51c528cafcdddb6bee433ae2c66a89
AlenaQA <alyona.trefilova@gmail.com>
2019-08-06 13:49:09 +0700

    Timemaster with Selenide.

---
+++ TimeMaster/specs/vacation.spec
@@ -0,0 +1,9 @@
+# Vacation
+
+## Create paid vacation
+
+* Click Vacations button
+* Click create vacation button
+* Set vacation dates
+* Click create vacation
+
--- TimeMaster/src/main/steps/VacationsSteps.java
+++ TimeMaster/src/main/steps/VacationsSteps.java
@@ -1,20 +1,31 @@
 package steps;
 
+import com.thoughtworks.gauge.Step;
+import timemaster.headerToolbarElement.HeaderToolbarElement;
 import timemaster.vacationsPage.VacationsPage;
 
 public class VacationsSteps {
 
     VacationsPage vacationsPage = new VacationsPage();
+    HeaderToolbarElement headerToolbarElement = new HeaderToolbarElement();
 
+    @Step("Click Vacations button")
+    public void clickVacationsButton() {
+        headerToolbarElement.clickVacations();
+    }
+
+    @Step("Click create vacation button")
     public void clickCreateVacationButton() {
         vacationsPage.click_createVacation();
     }
 
+    @Step("Set vacation dates")
     public void setVacationsDates(String dateFrom, String dateTo) {
         vacationsPage.set_date_from(dateFrom);
         vacationsPage.set_date_to(dateTo);
     }
 
+    @Step("Click create vacation")
     public void setUnpaidLeave() {
         vacationsPage.set_unpaid_leave();
     }
