ce9aa18558bac50e8a0de840ffe6831dfdf54d5d
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-08-06 14:03:19 +0700

    Merge branch 'master' of https://github.com/sidenisAcademy/private_academy into Selenide_isabekov
    # Conflicts:
    #	pom.xml
    #	src/test/java/PetTests1.java

--- TimeMaster/src/main/steps/LogoutSteps.java
+++ TimeMaster/src/main/steps/LogoutSteps.java
@@ -1,11 +1,12 @@
 package steps;
 
 
+import com.thoughtworks.gauge.Step;
 import timemaster.loginPage.LogoutPage;
 
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static context.utils.SetProperty.properties;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LogoutSteps {
     LogoutPage page = new LogoutPage();
@@ -14,5 +15,10 @@ public class LogoutSteps {
     }
     public void checkThatUserNotOnMainPage() {
         assertThat(url()).isNotEqualTo(properties.getProperty("TM_URI"));
+    }
+
+    @Step("test")
+    public void test(){
+        System.out.println("УРА");
     }
 }
