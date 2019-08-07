f47a2d27384c53341b3b8f56f34158863b883ac3
Emil Isabekov <Emil.Isabekov@sidenis.com>
2019-08-07 13:03:43 +0700

    Merge branch 'master' of https://github.com/sidenisAcademy/private_academy into Selenide_isabekov
    # Conflicts:
    #	pom.xml
    #	src/test/java/PetTests1.java

--- TimeMaster/src/main/steps/SettingsSteps.java
+++ TimeMaster/src/main/steps/SettingsSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import com.codeborne.selenide.Selenide;
 import timemaster.settingsPage.SettingsPage;
 
 public class SettingsSteps {
@@ -7,12 +8,13 @@ public class SettingsSteps {
 
     public void initializePage() {sp.loadSettingsTab();}
     public void setPreviousBreakValue() {sp.setPreviousBreak();}
-    public void setUserBreakValue() {sp.setBreakValue("0:30");}
+    public void setUserBreakValue() {sp.setBreakValue("0:60");}
     public void selectTimeZone() {sp.selectTimeZone("Krasnoyarsk");}
     public void setProjectList() {sp.selectProject("Sidenis");}
     public boolean isBreakInputEnabled() {
+        Selenide.sleep(10000);
         return sp.getBreak_input().isEnabled();
-    }
+    }  // isEnabled - свойство, которое может вернуть значение true или false, в нашем случае для GetBreak
     public String getBreakValue(){
         return sp.getBreak_input().getValue();
     }
